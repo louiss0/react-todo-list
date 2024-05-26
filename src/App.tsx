dde57270eb6ef0cd2887359143a27a9141c27d69
louiss0 <louisshelton0@gmail.com>
2024-05-26 17:08:55 -0400

    feat: style todo list app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import { useState } from 'react'
+import { ComponentProps, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='text-gray-900 bg-gray-50 h-screen '>
-      <h1 className='' >Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div data-wrapper="bg-reset" className='text-gray-900 bg-gray-50 h-screen '>
+      <div data-element="todo-list-app" className="mx-auto px-3 py-9 md:w-4/5 lg:w-3/5">
+
+        <div data-content className="flex flex-col items-center gap-6">
+          <h1 className='text-5xl' >To Do List App</h1>
+                
+        <div className="w-full sm:w-4/5 flex flex-col gap-4 p-4 rounded-md border-2 border-current">
+            <form data-element="todo-list-form">
+              <div data-content className="flex justify-between lg:justify-evenly items-center">
+                <input type="text" className="rounded-sm px-3 py-1" />
+                <Button className='bg-blue-300 rounded-sm px-6 py-2' >Submit</Button>
+              </div>
+            </form>
+            <div data-element="todo-list ">
+              <div data-element="todo-item" className="py-3 px-6">
+                <div data-content className="flex justify-between lg:justify-evenly items-center">
+                  <Button className='size-8 border rounded-full'>
+                    <i className='inline-block i-mdi:check'>
+                    </i>
+                    
+                      <div className="sr-only">Check</div>
+                  </Button>
+                  <input type="text" className='rounded-sm px-2' />
+                  <Button className='size-8 border rounded-full'>
+                    <i className='inline-block i-mdi:close'>
+                    </i>
+                    
+                      <div className="sr-only">Close</div>
+                  </Button>
+                </div>
+              </div>
+            </div>
+        </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
+}
+
+type ButtonProps = Omit<ComponentProps<"button">, "onClick"> & {
+  handleClick?:(...args:Array<any>)=> void
+}
+const combineStingsWithSpacesInBetween = (...args:Array<string>)=> args.join(" ") 
+
+const Button = ({ children, className, ...restProps }: ButtonProps) => {
+  return <button
+    className={
+      combineStingsWithSpacesInBetween(
+        "p-1",
+        "transition-transform duration-300 ease-in",
+        "hover:(brightness-150 scale-120)",
+        "focus:outline-current active:scale-90",
+        className ? className: '',
+      )
+  } {...restProps}>
+            {children}
+      </button>
+
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import 'uno.css'
+import 'virtual:uno.css'
 import 'virtual:unocss-devtools'
 import '@unocss/reset/tailwind.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
