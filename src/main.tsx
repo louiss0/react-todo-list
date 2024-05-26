3908c21c3edd72fb535397e826c9a3c23b68a303
louiss0 <louisshelton0@gmail.com>
2024-05-26 14:21:34 -0400

    chore: setup Unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-
+import 'uno.css'
+import 'virtual:unocss-devtools'
+import '@unocss/reset/tailwind.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
