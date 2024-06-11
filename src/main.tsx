e96b3577243934126905542f63e8f6f48212329e
louiss0 <louisshelton0@gmail.com>
2024-06-11 13:14:06 -0400

    fix: Move tailwind css reset to the top before all virtual stuff.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 import 'virtual:unocss-devtools'
-import '@unocss/reset/tailwind.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
